9237465a22553623264034cb2e7f168a6b052e85
Scott Murphy <scottmurphy09@gmail.com>
2018-01-17 22:07:47 -0600

    presentation-server added

---
+++ presentation-server/src/Lib.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE DataKinds       #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeOperators   #-}
+module Lib
+    ( startApp
+    , app
+    ) where
+
+import Network.Wai
+import Network.Wai.Handler.Warp
+import Servant
+import Presentation.Types
+
+
+startApp :: IO ()
+startApp = run 8080 app
+
+app :: Application
+app = serve api server
+
+
+server :: Server PresentationAPI
+server = return users
+
