0cc9ed367dbc23d5f4720ffd862255a9e649c332
Scott Murphy <scottmurphy09@gmail.com>
2018-01-20 15:48:13 -0600

    Sorting out the packages

--- presentation-types/src/Presentation/Types.hs
+++ presentation-types/src/Presentation/Types.hs
@@ -4,6 +4,13 @@
 {-# LANGUAGE TypeApplications #-}
 module Presentation.Types
     ( User(..)
+    , listApi
+    , ListAPI
+    , GameDate(..)
+    , Team (..)
+    , Score (..)
+    , Referee (..)
+    , GameResult (..)
     , PresentationAPI
     , api
     , users) where
@@ -12,6 +19,43 @@ import Data.Aeson
 import Data.Aeson.TH
 import Data.Proxy
 import Servant.API
+import Data.Time
+
+
+newtype GameDate= GameDate{unGameDate:: UTCTime }
+$(deriveJSON defaultOptions ''GameDate)
+
+
+newtype Team = Team {unTeam :: String}
+$(deriveJSON defaultOptions ''Team)
+
+newtype Score = Score {unScore :: Int}
+$(deriveJSON defaultOptions ''Score)
+
+
+data Referee = Referee
+  { _initial :: Char,
+    _lastName :: String}
+
+$(deriveJSON defaultOptions ''Referee)
+
+data GameResult = Home | Away
+
+$(deriveJSON defaultOptions ''GameResult)
+
+data Game = Game
+ {
+   _date :: GameDate,
+   _homeTeam :: Team,
+   _homeScore :: Score,
+   _awayTeam :: Team,
+   _awayScore :: Score,
+   _referee :: Referee,
+   _result :: GameResult
+ }
+
+
+$(deriveJSON defaultOptions ''Game)
 
 data User = User
   { userId        :: Int
@@ -23,8 +67,10 @@ $(deriveJSON defaultOptions ''User)
 
 type PresentationAPI = "users" :> Get '[JSON] [User]
 
+type ListAPI = "games" :> Get '[JSON] [Game]
 
-
+listApi :: Proxy ListAPI
+listApi = Proxy
 
 api :: Proxy PresentationAPI
 api = Proxy 
@@ -34,3 +80,5 @@ users :: [User]
 users = [ User 1 "Isaac" "Newton"
         , User 2 "Albert" "Einstein"
         ]
+
+
